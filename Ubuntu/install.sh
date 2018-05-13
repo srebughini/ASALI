4a2ab4d7204c5e59b16cc1615097bf1cc0d989d7
Stefano Rebughini <ste.rebu@gmail.com>
2018-05-13 12:12:06 +0200

    Update install.sh

--- Ubuntu/install.sh
+++ Ubuntu/install.sh
@@ -11,6 +11,6 @@ export EIGEN=$PWD/eigen-git-mirror
 export CANTERA=/usr/
 export ASALI_OS='-DASALI_ON_WINDOW=0'
 
-g++ -std=c++11 -Wall -Wextra  $ASALI_OS -pthread src/main.cpp src/Asali.C src/BatchInterface.C src/BatchEquations.C src/Ph1DbvpInterface.C src/Ph1DodeInterface.C src/Ph1DEquations.C src/canteraInterface.C src/speciesPopup.C src/asaliKinetic.C src/asaliProperties.C src/runBar.C src/asaliPlot.C -Isrc/ -I$EIGEN -I$CANTERA/include -L$CANTERA/lib `pkg-config gtkmm-3.0 --cflags --libs` -Wl,--start-group -lcantera -lblas -llapack -lcantera_fortran -lsundials_ida -lsundials_cvode -lsundials_cvodes -lsundials_nvecserial -Wl,--end-group -lgfortran -Wl,--no-as-needed -ldl -lpthread -lm -o Asali
+g++ -std=c++11 -Wall -Wextra  $ASALI_OS -pthread src/main.cpp src/Asali.C src/BatchInterface.C src/BatchEquations.C src/CstrInterface.C src/CstrEquations.C src/Ph1DbvpInterface.C src/Ph1DodeInterface.C src/Ph1DEquations.C src/canteraInterface.C src/speciesPopup.C src/asaliKinetic.C src/asaliProperties.C src/runBar.C src/asaliPlot.C -Isrc/ -I$EIGEN -I$CANTERA/include -L$CANTERA/lib `pkg-config gtkmm-3.0 --cflags --libs` -Wl,--start-group -lcantera -lblas -llapack -lcantera_fortran -lsundials_ida -lsundials_cvode -lsundials_cvodes -lsundials_nvecserial -Wl,--end-group -lgfortran -Wl,--no-as-needed -ldl -lpthread -lm -o Asali
 
 sudo ln -s $PWD/Asali /usr/bin/Asali
