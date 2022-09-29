c9711d3cb86fd8168782291bc9fe91b5ca74dd62
srebughini <ste.rebu@outlook.it>
2022-09-29 14:21:59 +0200

    Add transport properties results

---
+++ _results/operating-conditions.md
@@ -0,0 +1,25 @@
+---
+permalink: /results/operating-conditions/
+redirect_from: /docs/index.html
+---
+# **Operating conditions and composition**
+These are the operating conditions and composition used to estimate the **gas mixture transport properties**.
+<table class="table table-striped" id="input-table">
+  <tbody>
+    <tr>
+      <th scope="row">Temperature <i class="fa-regular fa-temperature-three-quarters"></i></th>
+      <td id="T"></td>
+      <td>K</td>
+    </tr>
+    <tr>
+      <th scope="row">Pressure <i class="fa-regular fa-gauge"></i></th>
+      <td id="P"></td>
+      <td>Pa</td>
+    </tr>
+    <tr>
+      <th scope="row">Name <i class="fa-regular fa-atom"></i></th>
+      <th >Mole fraction <i class="fa-regular fa-chart-pie"></i></th>
+      <th >Mass fraction <i class="fa-regular fa-chart-pie"></i></th>
+    </tr>
+  </tbody>
+</table>
--- assets/js/calculator.js
+++ assets/js/calculator.js
@@ -1,5 +1,6 @@
 var NSinput = 10;
 var mainDoc = window.document
+var destinationPageUrl = "https://srebughini.github.io/ASALI/results/operating-conditions"
 
 function readComposition() {
   const composition = {};
@@ -17,7 +18,6 @@ function readComposition() {
   }
   return composition;
 }
-
 
 function readTemperature() {
     return parseFloat(mainDoc.getElementById("T").value);
@@ -69,12 +69,14 @@ function estimateMixtureProperties() {
 
       //Extract output from the mixture object
       output = {
-      "properties": [
+      "transport": [
         {"name": "Molecular weight", "value": mixture.getMolecularWeight(), "ud": "kg/kmol"},
         {"name": "Density", "value": mixture.getDensity(), "ud": "kg/m<sup>3</sup>"},
         {"name": "Viscosity", "value": mixture.getViscosity(), "ud": "Pas"},
         {"name": "Diffusivity", "value": diff, "ud": "m<sup>2</sup>/s"},
-        {"name": "Thermal conductivity", "value": mixture.getThermalConductivity(), "ud": "W/m/K"},
+        {"name": "Thermal conductivity", "value": mixture.getThermalConductivity(), "ud": "W/m/K"}
+      ],
+      "thermo": [
         {"name": "Specific heat", "value": mixture.getMassSpecificHeat(), "ud": "J/kg/K"},
         {"name": "Enthalpy", "value": mixture.getMassEnthalpy(), "ud": "J/kg"},
         {"name": "Entropy", "value": mixture.getMassEntropy(), "ud": "J/kg/K"},
@@ -92,60 +94,11 @@ function estimateMixtureProperties() {
     }
   }
 }
-/*
-function gasMixtureProperties() {
-  let properties = estimateMixtureProperties();
-  if (properties.length > 0)
-  {
-    let propertiesWindow = window.open("output.html", "_blank");
-
-    //Opening a window is asynchronous
-    propertiesWindow.onload = function()
-    {
-      var propertiesDoc = propertiesWindow.document
-      let properties_id = "p";
-      let value_id = "v";
-      let unit_dimension_id = "ud";
-
-      for (let i = 0; i < properties.length; i++) {
-        
-        if (properties[i]["name"] == "Diffusivity")
-        {
-          propertiesDoc.getElementById(properties_id.concat(i)).innerHTML = properties[i]["name"]
-          let diff = ""
-          console.log(properties[i]["value"])
-          for (const [key, value] of Object.entries(properties[i]["value"])) {
-            diff = diff.concat(key)
-            diff = diff.concat(": ")
-            diff = diff.concat(value.toExponential(3))
-            diff = diff.concat("<br>")
-
-          }
-          propertiesDoc.getElementById(value_id.concat(i)).innerHTML = diff
-          propertiesDoc.getElementById(unit_dimension_id.concat(i)).innerHTML = properties[i]["ud"]
-        }
-        else
-        {
-          propertiesDoc.getElementById(properties_id.concat(i)).innerHTML = properties[i]["name"]
-          propertiesDoc.getElementById(value_id.concat(i)).innerHTML = properties[i]["value"].toExponential(3)
-          propertiesDoc.getElementById(unit_dimension_id.concat(i)).innerHTML = properties[i]["ud"]
-        }
-      }
-    }
-  }
-}*/
-
-
-function calculateEquilibrium() {
-    newWindow = window.open("", null, "height=200,width=400,status=yes,toolbar=no,menubar=no,location=center");  
-  
-    newWindow.document.write("<h1>Calculate Equilibrium: Under construction</h1>");
-}
 
 function runWebApp()
 {
     // Estimate mixture properties
-    let output = estimateMixtureProperties();
+    var output = estimateMixtureProperties();
 
     if (Object.keys(output).length > 0 )
     {
@@ -155,7 +108,7 @@ function runWebApp()
       let mass = output["composition"]["mass"];
 
       // Genere new window object
-      let transportWindow = window.open("https://srebughini.github.io/ASALI/results/transport-properties", "_blank");
+      var transportWindow = window.open(destinationPageUrl, "_blank");
 
       //Opening a window is asynchronous
       transportWindow.onload = function()
@@ -175,36 +128,33 @@ function runWebApp()
           moleCell.innerHTML = parseFloat(mole[i]).toExponential(3);
           massCell.innerHTML = parseFloat(mass[i]).toExponential(3);
         }
-          /*
-          let resultsDoc = resultsWindow.document
-          let properties_id = "p";
-          let value_id = "v";
-          let unit_dimension_id = "ud";
-    
-          for (let i = 0; i < properties.length; i++) {
-            
-            if (properties[i]["name"] == "Diffusivity")
-            {
-              propertiesDoc.getElementById(properties_id.concat(i)).innerHTML = properties[i]["name"]
-              let diff = ""
-              console.log(properties[i]["value"])
-              for (const [key, value] of Object.entries(properties[i]["value"])) {
-                diff = diff.concat(key)
-                diff = diff.concat(": ")
-                diff = diff.concat(value.toExponential(3))
-                diff = diff.concat("<br>")
-    
-              }
-              propertiesDoc.getElementById(value_id.concat(i)).innerHTML = diff
-              propertiesDoc.getElementById(unit_dimension_id.concat(i)).innerHTML = properties[i]["ud"]
-            }
-            else
-            {
-              propertiesDoc.getElementById(properties_id.concat(i)).innerHTML = properties[i]["name"]
-              propertiesDoc.getElementById(value_id.concat(i)).innerHTML = properties[i]["value"].toExponential(3)
-              propertiesDoc.getElementById(unit_dimension_id.concat(i)).innerHTML = properties[i]["ud"]
-            }
-          }*/
       }
     }
+}
+
+function updateTransport()
+{
+  if (Object.keys(output).length > 0 )
+  {
+    let properties = output["properties"];
+    let name = output["composition"]["name"];
+    let mole = output["composition"]["mole"];
+    let mass = output["composition"]["mass"];
+
+    let transportDoc = transportWindow.document;
+    let inputTable = transportDoc.getElementById("input-table")
+
+    transportDoc.getElementById("T").innerHTML = output["temperature"];
+    transportDoc.getElementById("P").innerHTML = output["pressure"];
+
+    for (let i = 0; i < name.length; i++) { 
+      let newRow = inputTable.insertRow(-1);
+      let nameCell = newRow.insertCell(0);
+      let moleCell = newRow.insertCell(1);
+      let massCell = newRow.insertCell(2);
+      nameCell.innerHTML = name[i];
+      moleCell.innerHTML = parseFloat(mole[i]).toExponential(3);
+      massCell.innerHTML = parseFloat(mass[i]).toExponential(3);
+    }
+  }
 }
