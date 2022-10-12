b50de939fcac91b1765f02fa26549a6aa7875ce8
srebughini <ste.rebu@outlook.it>
2022-10-12 15:13:42 +0200

    Add dropdown menu

--- assets/js/calculator.js
+++ assets/js/calculator.js
@@ -254,238 +254,236 @@ function runWebApp() {
   }
 }
 
-window.onload = function showResults() {
-  let destinationPageUrl = window.location.href.toString();
-  console.log(destinationPageUrl);
-  if (!destinationPageUrl.includes(webAppUrl))
-  {
-    console.log("soo qui");
-    // Get mixture properties from LocalStorage
-    let results = JSON.parse(localStorage.getItem(webAppResults));
+function showResults(destinationPageUrl) {
+  // Get mixture properties from LocalStorage
+  let results = JSON.parse(localStorage.getItem(webAppResults));
 
-    console.log(results);
-    console.log(Object.keys(results).length);
-
-    if (Object.keys(results).length > 0) {
-      showOperatingConditions(results, document);
-      if (destinationPageUrl.includes(transportPageUrl)) {
-        showTransportProperties(results, document);
-      }
-      else if (destinationPageUrl.includes(thermoPageUrl)) {
-        showThermoProperties(results, document);
-      }
-      else if (destinationPageUrl.includes(eqTPPageUrl)) {
-        showEquilibrium(results, document);
-      }
+  if (Object.keys(results).length > 0) {
+    showOperatingConditions(results, document);
+    if (destinationPageUrl.includes(transportPageUrl)) {
+      showTransportProperties(results, document);
+    }
+    else if (destinationPageUrl.includes(thermoPageUrl)) {
+      showThermoProperties(results, document);
+    }
+    else if (destinationPageUrl.includes(eqTPPageUrl)) {
+      showEquilibrium(results, document);
     }
   }
 }
 
 function showSpeciesList() {
-  let actualPageUrl = window.location.href.toString();
-  if (actualPageUrl.includes(webAppUrl))
-  {
-    let moleculesDict = {
-      "AR": {
-        elementsSymbol: ["Ar"],
-        numberOfAtoms: [1]
-      },
-      "HE": {
-        elementsSymbol: ["He"],
-        numberOfAtoms: [1]
-      },
-      "CH4": {
-        elementsSymbol: ["H", "C"],
-        numberOfAtoms: [4, 1]
-      },
-      "C3H8": {
-        elementsSymbol: ["H", "C"],
-        numberOfAtoms: [8, 3]
-      },
-      "C3H6": {
-        elementsSymbol: ["H", "C"],
-        numberOfAtoms: [6, 3]
-      },
-      "C2H4": {
-        elementsSymbol: ["H", "C"],
-        numberOfAtoms: [4, 2]
-      },
-      "C2H2": {
-        elementsSymbol: ["H", "C"],
-        numberOfAtoms: [2, 2]
-      },
-      "O2": {
-        elementsSymbol: ["O"],
-        numberOfAtoms: [2]
-      },
-      "N2": {
-        elementsSymbol: ["N"],
-        numberOfAtoms: [2]
-      },
-      "CO": {
-        elementsSymbol: ["C", "O"],
-        numberOfAtoms: [1, 1]
-      },
-      "CO2": {
-        elementsSymbol: ["C", "O"],
-        numberOfAtoms: [1, 2]
-      },
-      "H2O": {
-        elementsSymbol: ["H", "O"],
-        numberOfAtoms: [2, 1]
-      },
-      "H2O2": {
-        elementsSymbol: ["H", "O"],
-        numberOfAtoms: [2, 2]
-      },
-      "NO2": {
-        elementsSymbol: ["O", "N"],
-        numberOfAtoms: [2, 1]
-      },
-      "NO": {
-        elementsSymbol: ["O", "N"],
-        numberOfAtoms: [1, 1]
-      },
-      "N2O": {
-        elementsSymbol: ["O", "N"],
-        numberOfAtoms: [1, 2]
-      },
-      "NH3": {
-        elementsSymbol: ["H", "N"],
-        numberOfAtoms: [3, 1]
-      },
-      "CH2O": {
-        elementsSymbol: ["H", "C", "O"],
-        numberOfAtoms: [2, 1, 1]
-      },
-      "CH3OH": {
-        elementsSymbol: ["H", "C", "O"],
-        numberOfAtoms: [4, 1, 1]
-      },
-      "C6H6": {
-        elementsSymbol: ["H", "C"],
-        numberOfAtoms: [6, 6]
-      },
-      "C7H8": {
-        elementsSymbol: ["H", "C"],
-        numberOfAtoms: [8, 7]
-      },
-      "N2H4": {
-        elementsSymbol: ["H", "N"],
-        numberOfAtoms: [4, 2]
-      },
-      "CH3COOH": {
-        elementsSymbol: ["H", "C", "O"],
-        numberOfAtoms: [4, 2, 2]
-      },
-      "C2H5OH": {
-        elementsSymbol: ["H", "C", "O"],
-        numberOfAtoms: [6, 2, 1]
-      },
-      "CH3COCH3": {
-        elementsSymbol: ["H", "C", "O"],
-        numberOfAtoms: [6, 3, 1]
-      },
-      "MEK": {
-        elementsSymbol: ["H", "C", "O"],
-        numberOfAtoms: [8, 4, 1]
-      },
-      "NC4H8": {
-        elementsSymbol: ["H", "C"],
-        numberOfAtoms: [8, 4]
-      },
-      "IC4H8": {
-        elementsSymbol: ["H", "C"],
-        numberOfAtoms: [8, 4]
-      },
-      "IC4H10": {
-        elementsSymbol: ["H", "C"],
-        numberOfAtoms: [10, 4]
-      },
-      "NC4H10": {
-        elementsSymbol: ["H", "C"],
-        numberOfAtoms: [10, 4]
-      },
-      "N1C4H9OH": {
-        elementsSymbol: ["H", "C", "O"],
-        numberOfAtoms: [10, 4, 1]
-      },
-      "N2C4H9OH": {
-        elementsSymbol: ["H", "C", "O"],
-        numberOfAtoms: [10, 4, 1]
-      },
-      "TC4H9OH": {
-        elementsSymbol: ["H", "C", "O"],
-        numberOfAtoms: [10, 4, 1]
-      },
-      "IC4H9OH": {
-        elementsSymbol: ["H", "C", "O"],
-        numberOfAtoms: [10, 4, 1]
-      },
-      "CYC5H8": {
-        elementsSymbol: ["H", "C"],
-        numberOfAtoms: [8, 5]
-      },
-      "C5H8": {
-        elementsSymbol: ["H", "C"],
-        numberOfAtoms: [8, 5]
-      },
-      "C6H5OH": {
-        elementsSymbol: ["H", "C", "O"],
-        numberOfAtoms: [6, 6, 1]
-      },
-      "C6H5CHO": {
-        elementsSymbol: ["H", "C", "O"],
-        numberOfAtoms: [6, 7, 1]
-      },
-      "NC7H14": {
-        elementsSymbol: ["H", "C"],
-        numberOfAtoms: [14, 7]
-      },
-      "NC7H16": {
-        elementsSymbol: ["H", "C"],
-        numberOfAtoms: [16, 7]
-      },
-      "XYLENE": {
-        elementsSymbol: ["H", "C"],
-        numberOfAtoms: [10, 8]
-      },
-      "C6H5C2H3": {
-        elementsSymbol: ["H", "C"],
-        numberOfAtoms: [8, 8]
-      },
-      "IC8H18": {
-        elementsSymbol: ["H", "C"],
-        numberOfAtoms: [18, 8]
-      },
-      "NC10H22": {
-        elementsSymbol: ["H", "C"],
-        numberOfAtoms: [22, 10]
-      },
-      "BIPHENYL": {
-        elementsSymbol: ["H", "C"],
-        numberOfAtoms: [10, 12]
-      },
-      "H2": {
-        elementsSymbol: ["H"],
-        numberOfAtoms: [2]
-      },
-      "C2H6": {
-        elementsSymbol: ["H", "C"],
-        numberOfAtoms: [6, 2]
-      }
-    }
-
-    let specieNames = Object.keys(moleculesDict);
-
-    for (let i = 0; i < NSinput; i++) {
-      let select_obj = document.getElementById(name_id_prefix.concat(i + 1));
-      for (let j = 0; j < specieNames.length; j++) {
-        let opt = document.createElement('option');
-        opt.value = specieNames[j];
-        opt.text = specieNames[j];
-        select_obj.add(opt);
-      }
+  let moleculesDict = {
+    "AR": {
+      elementsSymbol: ["Ar"],
+      numberOfAtoms: [1]
+    },
+    "HE": {
+      elementsSymbol: ["He"],
+      numberOfAtoms: [1]
+    },
+    "CH4": {
+      elementsSymbol: ["H", "C"],
+      numberOfAtoms: [4, 1]
+    },
+    "C3H8": {
+      elementsSymbol: ["H", "C"],
+      numberOfAtoms: [8, 3]
+    },
+    "C3H6": {
+      elementsSymbol: ["H", "C"],
+      numberOfAtoms: [6, 3]
+    },
+    "C2H4": {
+      elementsSymbol: ["H", "C"],
+      numberOfAtoms: [4, 2]
+    },
+    "C2H2": {
+      elementsSymbol: ["H", "C"],
+      numberOfAtoms: [2, 2]
+    },
+    "O2": {
+      elementsSymbol: ["O"],
+      numberOfAtoms: [2]
+    },
+    "N2": {
+      elementsSymbol: ["N"],
+      numberOfAtoms: [2]
+    },
+    "CO": {
+      elementsSymbol: ["C", "O"],
+      numberOfAtoms: [1, 1]
+    },
+    "CO2": {
+      elementsSymbol: ["C", "O"],
+      numberOfAtoms: [1, 2]
+    },
+    "H2O": {
+      elementsSymbol: ["H", "O"],
+      numberOfAtoms: [2, 1]
+    },
+    "H2O2": {
+      elementsSymbol: ["H", "O"],
+      numberOfAtoms: [2, 2]
+    },
+    "NO2": {
+      elementsSymbol: ["O", "N"],
+      numberOfAtoms: [2, 1]
+    },
+    "NO": {
+      elementsSymbol: ["O", "N"],
+      numberOfAtoms: [1, 1]
+    },
+    "N2O": {
+      elementsSymbol: ["O", "N"],
+      numberOfAtoms: [1, 2]
+    },
+    "NH3": {
+      elementsSymbol: ["H", "N"],
+      numberOfAtoms: [3, 1]
+    },
+    "CH2O": {
+      elementsSymbol: ["H", "C", "O"],
+      numberOfAtoms: [2, 1, 1]
+    },
+    "CH3OH": {
+      elementsSymbol: ["H", "C", "O"],
+      numberOfAtoms: [4, 1, 1]
+    },
+    "C6H6": {
+      elementsSymbol: ["H", "C"],
+      numberOfAtoms: [6, 6]
+    },
+    "C7H8": {
+      elementsSymbol: ["H", "C"],
+      numberOfAtoms: [8, 7]
+    },
+    "N2H4": {
+      elementsSymbol: ["H", "N"],
+      numberOfAtoms: [4, 2]
+    },
+    "CH3COOH": {
+      elementsSymbol: ["H", "C", "O"],
+      numberOfAtoms: [4, 2, 2]
+    },
+    "C2H5OH": {
+      elementsSymbol: ["H", "C", "O"],
+      numberOfAtoms: [6, 2, 1]
+    },
+    "CH3COCH3": {
+      elementsSymbol: ["H", "C", "O"],
+      numberOfAtoms: [6, 3, 1]
+    },
+    "MEK": {
+      elementsSymbol: ["H", "C", "O"],
+      numberOfAtoms: [8, 4, 1]
+    },
+    "NC4H8": {
+      elementsSymbol: ["H", "C"],
+      numberOfAtoms: [8, 4]
+    },
+    "IC4H8": {
+      elementsSymbol: ["H", "C"],
+      numberOfAtoms: [8, 4]
+    },
+    "IC4H10": {
+      elementsSymbol: ["H", "C"],
+      numberOfAtoms: [10, 4]
+    },
+    "NC4H10": {
+      elementsSymbol: ["H", "C"],
+      numberOfAtoms: [10, 4]
+    },
+    "N1C4H9OH": {
+      elementsSymbol: ["H", "C", "O"],
+      numberOfAtoms: [10, 4, 1]
+    },
+    "N2C4H9OH": {
+      elementsSymbol: ["H", "C", "O"],
+      numberOfAtoms: [10, 4, 1]
+    },
+    "TC4H9OH": {
+      elementsSymbol: ["H", "C", "O"],
+      numberOfAtoms: [10, 4, 1]
+    },
+    "IC4H9OH": {
+      elementsSymbol: ["H", "C", "O"],
+      numberOfAtoms: [10, 4, 1]
+    },
+    "CYC5H8": {
+      elementsSymbol: ["H", "C"],
+      numberOfAtoms: [8, 5]
+    },
+    "C5H8": {
+      elementsSymbol: ["H", "C"],
+      numberOfAtoms: [8, 5]
+    },
+    "C6H5OH": {
+      elementsSymbol: ["H", "C", "O"],
+      numberOfAtoms: [6, 6, 1]
+    },
+    "C6H5CHO": {
+      elementsSymbol: ["H", "C", "O"],
+      numberOfAtoms: [6, 7, 1]
+    },
+    "NC7H14": {
+      elementsSymbol: ["H", "C"],
+      numberOfAtoms: [14, 7]
+    },
+    "NC7H16": {
+      elementsSymbol: ["H", "C"],
+      numberOfAtoms: [16, 7]
+    },
+    "XYLENE": {
+      elementsSymbol: ["H", "C"],
+      numberOfAtoms: [10, 8]
+    },
+    "C6H5C2H3": {
+      elementsSymbol: ["H", "C"],
+      numberOfAtoms: [8, 8]
+    },
+    "IC8H18": {
+      elementsSymbol: ["H", "C"],
+      numberOfAtoms: [18, 8]
+    },
+    "NC10H22": {
+      elementsSymbol: ["H", "C"],
+      numberOfAtoms: [22, 10]
+    },
+    "BIPHENYL": {
+      elementsSymbol: ["H", "C"],
+      numberOfAtoms: [10, 12]
+    },
+    "H2": {
+      elementsSymbol: ["H"],
+      numberOfAtoms: [2]
+    },
+    "C2H6": {
+      elementsSymbol: ["H", "C"],
+      numberOfAtoms: [6, 2]
     }
   }
+
+  let specieNames = Object.keys(moleculesDict);
+
+  for (let i = 0; i < NSinput; i++) {
+    let select_obj = document.getElementById(name_id_prefix.concat(i + 1));
+    for (let j = 0; j < specieNames.length; j++) {
+      let opt = document.createElement('option');
+      opt.value = specieNames[j];
+      opt.text = specieNames[j];
+      select_obj.add(opt);
+    }
+  }
+}
+
+window.onload = function updatePage() {
+  let actualPageUrl = window.location.href.toString();
+  if (actualPageUrl.includes(webAppUrl)) {
+    showSpeciesList();
+  }
+  else {
+    showResults(actualPageUrl);
+  }
+
 }
