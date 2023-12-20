127a9d268c52f3d53dec80d73ddfc49f4484ecd4
srebughini <ste.rebu@outlook.it>
2023-12-20 13:52:31 +0100

    Fix minor bug

--- assets/js/jasali.js
+++ assets/js/jasali.js
@@ -83860,6 +83860,7 @@
             let tr = _temperature / _LJpotential;
             let dr = 1.e06 * 0.5 * Math.pow(_dipole, 2.) / (_LJpotential * _utils.Parameters.k * Math.pow(_LJdiameter, 3.));
             let sigma = (0, _omega.CollisionIntegral22)(tr, dr);
+            console.log(sigma, tr, dr);
             _mu = 1e-05 * (5 / 16) * Math.sqrt(_utils.Parameters.pi * _utils.Parameters.k * _temperature * _molecularWeight * 1.66054) / (_utils.Parameters.pi * sigma * Math.pow(_LJdiameter, 2));
             _mu_update = false;
           }
@@ -84802,7 +84803,7 @@
         } else if (value > array[value.length - 1]) {
           return [value.length - 2, value.length - 1];
         } else {
-          for (let i = 0; i < array.length - 2; i++) {
+          for (let i = 0; i < array.length - 1; i++) {
             if (value >= array[i] && value < array[i + 1]) {
               return [i, i + 1];
             }
