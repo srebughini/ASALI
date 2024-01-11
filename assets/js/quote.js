847cba13cee54c88e9ee4007623aba042b39fa92
srebughini <ste.rebu@outlook.it>
2024-01-11 16:25:42 +0100

    Add beer quotes

--- assets/js/quote.js
+++ assets/js/quote.js
@@ -1,6 +1,6 @@
 function getQuote() {
   let randomQuote = beerq.BeerQuote().getRandomQuote();
-  document.getElementById("quote").innerHTML = "<i>" + randomQuote.quote + "</i>br[" + randomQuote.author + "]";
+  document.getElementById("quote").innerHTML = "<i>" + randomQuote.quote + "</i><br>[" + randomQuote.author + "]";
 }
 
 setInterval(getQuote, 10000); /*Call it here*/
