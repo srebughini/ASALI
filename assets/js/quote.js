c39ac49d199dac0f804fdd08e8b8f2313d84982c
srebughini <ste.rebu@outlook.it>
2024-02-05 14:32:17 +0100

    Trying to fix bug

--- assets/js/quote.js
+++ assets/js/quote.js
@@ -1,6 +1,10 @@
 function getQuote() {
-  let randomQuote = beerq.BeerQuote("eng").getRandomQuote();
-  document.getElementById("quote").innerHTML = "<br><i>" + randomQuote.quote + "<br>[" + randomQuote.author + "]</i>";
+  let quote = document.getElementById("quote");
+  if (quote)
+  {
+    let randomQuote = beerq.BeerQuote("eng").getRandomQuote();
+    quote.innerHTML = "<br><i>" + randomQuote.quote + "<br>[" + randomQuote.author + "]</i>";
+  }
 }
 
 setInterval(getQuote, 10000); /*Call it here*/
