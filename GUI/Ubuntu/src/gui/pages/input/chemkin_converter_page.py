eca68254d7c29faa6ae362f6ce13491c4288d758
srebughini <ste.rebu@gmail.com>
2025-06-04 09:26:04 +0200

    Add CHEMKIN to CANTERA converter

--- GUI/Ubuntu/src/gui/pages/input/chemkin_converter_page.py
+++ GUI/Ubuntu/src/gui/pages/input/chemkin_converter_page.py
@@ -1,4 +1,6 @@
 from PyQt5 import uic
+from PyQt5.QtWidgets import QLabel
+from asali.utils.cantera_file_converter import CanteraFileConverter
 
 from src.config.app import AppConfig
 from src.core.data_keys import DataKeys
@@ -36,6 +38,36 @@ class ChemkinConverterInputPage(BasicPage):
         action_box = self.find_widget(ChemkinConverterInputPageComponents.ACTION)
         action_box.setCurrentText(ChemkinEditorAction.CONVERTER.value)
 
+        for widget_enum in [ChemkinConverterInputPageComponents.THERMO_BUTTON,
+                            ChemkinConverterInputPageComponents.TRANSPORT_BUTTON,
+                            ChemkinConverterInputPageComponents.KINETIC_BUTTON,
+                            ChemkinConverterInputPageComponents.SURFACE_BUTTON]:
+            widget = self.find_widget(widget_enum)
+            widget_text = widget.text().replace("\u2714 ", "")
+            widget.setText(widget_text)
+
+        for key in [DataKeys.TRANSPORT_FILE_PATH,
+                    DataKeys.THERMO_FILE_PATH,
+                    DataKeys.KINETIC_FILE_PATH,
+                    DataKeys.SURFACE_FILE_PATH]:
+            self.data_store.reset_data(key)
+
+    def add_tick_to_button(self, button_enum) -> None:
+        """
+        Add tick to the button text
+        Parameters
+        ----------
+        button_enum: Enum
+            Button text
+
+        Returns
+        -------
+
+        """
+        button = self.find_widget(button_enum)
+        button_text = button.text().replace("\u2714 ", "")
+        button.setText(f"\u2714 {button_text}")
+
     def update_combo_boxes(self) -> None:
         """
         Update combo boxes
@@ -58,7 +90,7 @@ class ChemkinConverterInputPage(BasicPage):
         back_button.clicked.connect(lambda: self.page_switched.emit(AppConfig.MAIN_INPUT_PAGE))
 
         calculate_button = self.find_widget(ChemkinConverterInputPageComponents.RUN_BUTTON)
-        calculate_button.clicked.connect(self.show_data)
+        calculate_button.clicked.connect(self.convert)
 
         thermo_button = self.find_widget(ChemkinConverterInputPageComponents.THERMO_BUTTON)
         thermo_button.clicked.connect(self.load_thermo_file)
@@ -100,6 +132,7 @@ class ChemkinConverterInputPage(BasicPage):
 
         if file_path:
             self.data_store.update_data(DataKeys.THERMO_FILE_PATH, file_path)
+            self.add_tick_to_button(ChemkinConverterInputPageComponents.THERMO_BUTTON)
 
     def load_transport_file(self) -> None:
         """
@@ -113,6 +146,7 @@ class ChemkinConverterInputPage(BasicPage):
 
         if file_path:
             self.data_store.update_data(DataKeys.TRANSPORT_FILE_PATH, file_path)
+            self.add_tick_to_button(ChemkinConverterInputPageComponents.TRANSPORT_BUTTON)
 
     def load_gas_kinetic_file(self) -> None:
         """
@@ -126,6 +160,7 @@ class ChemkinConverterInputPage(BasicPage):
 
         if file_path:
             self.data_store.update_data(DataKeys.KINETIC_FILE_PATH, file_path)
+            self.add_tick_to_button(ChemkinConverterInputPageComponents.KINETIC_BUTTON)
 
     def load_surface_kinetic_file(self) -> None:
         """
@@ -139,21 +174,47 @@ class ChemkinConverterInputPage(BasicPage):
 
         if file_path:
             self.data_store.update_data(DataKeys.SURFACE_FILE_PATH, file_path)
+            self.add_tick_to_button(ChemkinConverterInputPageComponents.SURFACE_BUTTON)
 
-    def read_data(self) -> None:
+    def convert(self) -> None:
         """
-        Update data store with temperature, composition, pressure
+        Convert Chemkin file to Cantera file
         Returns
         -------
 
         """
-        pass
+        kinetic_file_path = self.data_store.get_data(DataKeys.KINETIC_FILE_PATH)
+        thermo_file_path = self.data_store.get_data(DataKeys.THERMO_FILE_PATH)
+        transport_file_path = self.data_store.get_data(DataKeys.TRANSPORT_FILE_PATH)
+        surface_file_path = self.data_store.get_data(DataKeys.SURFACE_FILE_PATH)
 
-    def show_data(self) -> None:
-        """
-        Update shown data
-        Returns
-        -------
+        if len(thermo_file_path) < 1:
+            self.dialog_handler.error_message(QLabel("Missing Chemkin thermodynamic file"))
+            return None
 
-        """
-        pass
+        if len(transport_file_path) < 1:
+            self.dialog_handler.error_message(QLabel("Missing Chemkin transport file"))
+            return None
+
+        if len(kinetic_file_path) < 1:
+            self.dialog_handler.error_message(QLabel("Missing Chemkin kinetic file"))
+            return None
+
+        if len(surface_file_path) < 1:
+            surface_file_path = None
+
+        output_file_path = self.dialog_handler.save_file(AppConfig.CANTERA_FILE_SAVE.value,
+                                                         AppConfig.CANTERA_FILE_TYPE.value)
+
+        if output_file_path:
+            try:
+                CanteraFileConverter.from_chemkin_to_yaml(kinetic_file_path=kinetic_file_path,
+                                                          thermodynamic_file_path=thermo_file_path,
+                                                          transport_file_path=transport_file_path,
+                                                          surface_file_path=surface_file_path,
+                                                          output_file_path=output_file_path,
+                                                          validation=False)
+            except Exception as e:
+                raise Exception(e)
+            else:
+                self.dialog_handler.done_message(QLabel("Cantera file saved!"))
